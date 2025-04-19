6905006da9667b4b5a815d086c4493e9f368fedf
nidhiatgithub <34266670+nidhiatgithub@users.noreply.github.com>
2025-04-19 07:54:41 +0530

    scorefinder

--- src/mockInterview/ScoreFinder.java
+++ src/mockInterview/ScoreFinder.java
@@ -2,15 +2,27 @@ package src.mockInterview;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ScoreFinder {
     public static void main(String[] args) {
-        int[] shots = {5, 3, 10};
+        int[] shots = {3, 5, 10};
         int score = 20;
         int[] ways = new int[7];
 
 //        scoreGeneratorRaw(shots, score);
-        scoreGenerator(shots, ways, score, 0);
+//        scoreGenerator(shots, ways, score, 0);
+//        AtomicInteger totalWays = new AtomicInteger(0);
+//        waysFinder(shots, score, totalWays);
+//        System.out.println("Total no of ways: "+ totalWays);
+
+        System.out.println("take No take approach counter:"+ takeNoTakeApproachCountRecursive(score, shots,0));
+
+//        int[][] memo = new int[score+1][shots.length];
+//        for (int[] row: memo) {
+//            Arrays.fill(row,-1);
+//        }
+//        System.out.println("take No take recursive approach memo approach counter:"+ takeNoTakeApproachCountRecursiveMemo(score, shots,0, memo));
     }
 
 //    private static void scoreGeneratorRaw(int[] shots, int score)  {
@@ -22,20 +34,31 @@ public class ScoreFinder {
 //        }
 //    }
 
+    private static void waysFinder(int[] shots, int score, AtomicInteger totalWays) {
+        if(score == 0)
+        {
+            totalWays.set(totalWays.intValue()+1);
+            return;
+        }
+        if(score<0)
+            return;
+        for(int i=0;i<shots.length-1;i++){
+            waysFinder(shots, score-shots[i], totalWays);
+        }
+    }
 
     /**
      *
      * generates all permutations of ways of scoring
-     *
+     * , [3, 3, 3, 3, 3, 5, 0]
+     *  , [3, 3, 3, 3, 5, 3, 0]
+     *  , [3, 3, 3, 5, 3, 3, 0]
+     *  , [3, 3, 5, 3, 3, 3, 0]
+     *  , [3, 5, 3, 3, 3, 3, 0]
+     *  , [5, 3, 3, 3, 3, 3, 0]
      *  , [5, 5, 5, 5, 0, 0, 0]
      *  , [5, 5, 10, 0, 0, 0, 0]
-     *  , [5, 3, 3, 3, 3, 3, 0]
      *  , [5, 10, 5, 0, 0, 0, 0]
-     *  , [3, 5, 3, 3, 3, 3, 0]
-     *  , [3, 3, 5, 3, 3, 3, 0]
-     *  , [3, 3, 3, 5, 3, 3, 0]
-     *  , [3, 3, 3, 3, 5, 3, 0]
-     *  , [3, 3, 3, 3, 3, 5, 0]
      *  , [10, 5, 5, 0, 0, 0, 0]
      *  , [10, 10, 0, 0, 0, 0, 0]
      * @param shots
@@ -44,7 +67,7 @@ public class ScoreFinder {
      * @param index
      */
     private static void scoreGenerator(int[] shots, int[] ways, int totalSum, int index) {
-//        System.out.println("score generator: "+ Arrays.toString(ways) + ", " +totalSum + " , " +index);
+        System.out.println("score generator: "+ Arrays.toString(ways) + ", " +totalSum + " , " +index);
         if(totalSum==0){
             System.out.println(" , "+ Arrays.toString(ways));
             return;
@@ -54,7 +77,7 @@ public class ScoreFinder {
             return;
         }
 
-        for(int i=0; i<shots.length; i++)
+        for(int i=0; i<shots.length-1; i++)
         {
             ways[index]=shots[i];
             for(int j=index+1; j<ways.length; j++)
@@ -62,5 +85,35 @@ public class ScoreFinder {
             scoreGenerator(shots, ways, totalSum-shots[i], index+1);
         }
     }
-}
 //3 3 3 3 3 3
+
+private static int takeNoTakeApproachCountRecursive(int total, int[] shots, int index) {
+    if(total==0)
+        return 1;
+    if(total<0 || index==shots.length)
+        return 0;
+
+    System.out.println("inside take:" + (total-shots[index]) +", "+index);
+    int take = takeNoTakeApproachCountRecursive(total-shots[index], shots, index);
+
+
+    System.out.println("inside notake:" + total +", "+(index+1));
+    int noTake = takeNoTakeApproachCountRecursive(total, shots, index+1);
+    return take+noTake;
+}
+
+    private static int takeNoTakeApproachCountRecursiveMemo(int total, int[] shots, int index, int[][] memo) {
+        if(total==0)
+            return 1;
+        if(total<0 || index==shots.length)
+            return 0;
+
+        if(memo[total][index] != -1)
+            return memo[total][index];
+//        System.out.println("inside takeNoTake:" + total +", "+ Arrays.toString(shots)+", "+index);
+
+        int take = takeNoTakeApproachCountRecursiveMemo(total-shots[index], shots, index, memo);
+        int noTake = takeNoTakeApproachCountRecursiveMemo(total, shots, index+1, memo);
+        return memo[total][index]=take+noTake;
+    }
+}
